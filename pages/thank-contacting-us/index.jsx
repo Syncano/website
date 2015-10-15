5e665490865cb70d1d0e2a5558b27540b9082752
Patryk Kopycinski <contact@patrykkopycinski.com>
2015-10-15 21:49:39 +0200

    Improve thank you page

--- pages/thank-contacting-us/index.jsx
+++ pages/thank-contacting-us/index.jsx
@@ -14,14 +14,15 @@ export default React.createClass({
     let SEO = this.getSEOConfig();
 
     return (
-      <div className="contact" style={{paddingTop: 90}}>
+      <div className="contact" style={{paddingTop: 90, paddingBottom: 90}}>
         <Helmet title={SEO.title}/>
         <div className="container">
-          <div className="col-md-6">
-            <h1>Thank you! Your message has been received.</h1>
+          <div className="col-md-7">
+            <h1>Thank you!<br/>Your message has been received.</h1>
+            <br/>
             <p>We’ll get back to you soon. In the mean time, check out some of our recent <Link to="blog-template">blog articles.</Link></p>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-5">
             <img src="new_york_office_hydra_420px.jpg" alt="New York City" />
           </div>
         </div>
