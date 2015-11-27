8cf4cd89eb96a61f84cfc90d2cafcbad8bcbe65a
Patryk Kopycinski <contact@patrykkopycinski.com>
2015-11-27 17:45:57 +0100

    [WEB-92] Add CA office address to website

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -43,22 +43,29 @@ export default React.createClass({
         <div className="white" style={{paddingTop: 40, paddingBottom: 40}}>
           <div className="container">
             <div className="row">
-              <div className="col-md-4 col-sm-4 text-center location">
-                <img src="nyc.png" />
-                <h2>USA</h2>
+              <div className="col-sm-3 text-center location">
+                <img src="nyc.jpg" alt="nyc"/>
+                <h2>USA - East Coast</h2>
                 <p><a href="https://www.google.com/maps/place/120+E+23rd+St,+New+York,+NY+10010/@40.739496,-73.98572,17z/data=!3m1!4b1!4m2!3m1!1s0x89c259a0de37a537:0x60b1997940b9a35f" target="_blank">120 East 23rd St.</a></p>
                 <p>5th Floor</p>
                 <p>New York, NY 10010</p>
               </div>
-              <div className="col-md-4 col-sm-4 text-center location">
-                <img src="poland.png" />
+              <div className="col-sm-3 text-center location">
+                <img src="sanleandro.jpg" alt="sanleandro"/>
+                <h2>USA - West Coast</h2>
+                <p><a href="https://www.google.com/maps/place/1933+Davis+St+%23249,+San+Leandro,+CA+94577,+USA/@37.7163316,-122.1817367,17z/data=!3m1!4b1!4m2!3m1!1s0x808f8563fc8cbdc9:0xb2f270a9734d0b6b" target="_blank">1933 Davis St.</a></p>
+                <p>#249</p>
+                <p>94577 San Leandro, CA</p>
+              </div>
+              <div className="col-sm-3 text-center location">
+                <img src="warsaw.jpg" alt="warsaw"/>
                 <h2>POLAND</h2>
                 <p><a href="https://www.google.com/maps/place/Aleje+Jerozolimskie+81,+Warszawa,+Poland/@52.2269719,21.001542,17z/data=!3m1!4b1!4m2!3m1!1s0x471ecc928b79810d:0xdfe2f37dc4130825" target="_blank">Al. Jerozolimskie 81</a></p>
                 <p>02-001 Warsaw</p>
                 <p>Poland</p>
               </div>
-              <div className="col-md-4 col-sm-4 text-center location">
-                <img src="norway.png" />
+              <div className="col-sm-3 text-center location">
+                <img src="oslo.jpg" alt="oslo"/>
                 <h2>NORWAY</h2>
                 <p><a href="https://www.google.com/maps/place/Stortingsgata+30,+0161+Oslo,+Norway/@59.9147703,10.7317376,17z/data=!3m1!4b1!4m2!3m1!1s0x46416e7e12fff935:0xda8a78ba0761b31" target="_blank">Stortingsgata 30</a></p>
                 <p>0161 Oslo</p>
