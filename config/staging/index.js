71e3f45387fb74a8ca09d7e26f916a57ce41d82d
Patryk Kopycinski <contact@patrykkopycinski.com>
2015-12-14 06:51:18 +0100

    [WEB-76] Create Google promoted signup on hero slide

--- config/staging/index.js
+++ config/staging/index.js
@@ -4,6 +4,6 @@ export default {
   dashboardUrl: 'https://dashboard.syncano.io/',
   hello: {
     google: '263058036063-8h3r2mn9le5mllf0q3je6gruv14u2ms3.apps.googleusercontent.com',
-    redirect_uri: 'https://www.syncano.rocks/'
+    redirect_uri: 'https://syncano.rocks/'
   }
 };
