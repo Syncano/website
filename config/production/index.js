21cbeee41f3f24d38a036867c0eff005d50e3196
Patryk Kopycinski <contact@patrykkopycinski.com>
2015-12-14 07:16:22 +0100

    [WEB-76] Create Google promoted signup on hero slide

--- config/production/index.js
+++ config/production/index.js
@@ -1,5 +1,9 @@
 export default {
   analyticsKey: 'fLDtpYXRjFYnHlp1gvzl4I3Gv8gDoQ8m',
   apiUrl: 'https://api.syncano.io/',
-  dashboardUrl: 'https://dashboard.syncano.io/'
+  dashboardUrl: 'https://dashboard.syncano.io/',
+  hello: {
+    google: '263058036063-ubii4m8j1jlhea23cc39774v7fuaa4gi.apps.googleusercontent.com',
+    redirect_uri: 'https://wwww.syncano.io/'
+  }
 };
