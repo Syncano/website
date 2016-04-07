e366badcdc63aaedb3be0c97a6d0efdbfd2db3f2
Wojciech Pasiński <wpasinski@gmail.com>
2016-04-07 13:42:43 +0200

    [WEB-245] Implement libraries icons section

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
-import Hero from '../components/Hero';
 import { Link } from 'react-router';
+import Hero from '../components/Hero';
+import Platforms from '../components/Platforms';
 
 export default () => (
   <DocumentTitle title="Syncano">
-    <Hero
-      headline="Rethink how you build apps"
-      textline="A complete platform to orchestrate your app's ecosystem."
-      variant="home"
-    >
-      <Link
-        to="/about/"
-        className="button button-large button-featured"
+    <div>
+      <Hero
+        headline="Rethink how you build apps"
+        textline="A complete platform to orchestrate your app's ecosystem."
+        variant="home"
       >
-        Get Started for Free
-      </Link>
-    </Hero>
+        <Link
+          to="/about/"
+          className="button button-large button-featured"
+        >
+          Get Started for Free
+        </Link>
+      </Hero>
+      <Platforms />
+    </div>
   </DocumentTitle>
 );
