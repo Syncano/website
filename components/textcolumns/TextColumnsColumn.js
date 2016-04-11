f014f8062ad9077f17eae79a3f819921a33695a4
Wojciech Pasiński <wpasinski@gmail.com>
2016-04-11 16:30:29 +0200

    [WEB-244] removing unnecessary children, reordering SCSS colors, pascal case for component folder names

--- components/textcolumns/TextColumnsColumn.js
+++ components/textcolumns/TextColumnsColumn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default ({icon, headline, text, children}) => {
+export default ({icon, headline, text}) => {
   return (
     <div className="text-columns__column">
       <div className="columns__column__headline">
