1ed58fe8d3e6f3dc33401338b228b1c961db4c65
Wojciech Pasiński <wpasinski@gmail.com>
2016-04-11 19:11:16 +0200

    [WEB-239] Implement section with image sticked to the bottom

--- components/index.js
+++ components/index.js
@@ -1,11 +1,13 @@
 import CTASection from './CTASection';
 import Hero from './Hero';
 import Platforms from './Platforms';
+import TextWithBottomImage from './TextWithBottomImage';
 import TextWithImage from './TextWithImage';
 
 export default {
   CTASection,
   Hero,
   Platforms,
+  TextWithBottomImage,
   TextWithImage
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { Link } from 'react-router';
-import { CTASection, Hero, Platforms, TextWithImage } from '../components';
+import { CTASection, Hero, Platforms, TextWithBottomImage, TextWithImage } from '../components';
 
 export default () => (
   <DocumentTitle title="Syncano">
@@ -74,6 +74,23 @@ export default () => (
           More About Scripts
         </Link>
       </TextWithImage>
+      <TextWithBottomImage
+        headline="Get answers from our developers"
+        textline="Our engineers specialize in the tricky and technical investigations of our platform. When you have questions, you’ll get timely answers from our team."
+        image={
+          <img
+            src={require('../images/robot.svg')}
+            alt="robot"
+          />
+        }
+      >
+        <Link
+          to="/about/"
+          className="button"
+        >
+          Get Support
+        </Link>
+      </TextWithBottomImage>
       <CTASection>
         <Link
           to="/about/"
