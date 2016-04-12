9135d96821b15421c13ca234e7dbe4cab2e9177b
Wojciech Pasiński <wpasinski@gmail.com>
2016-04-12 22:17:07 +0200

    [WEB-246] Implement top navigation

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { TopBar } from '../components';
 import 'normalize.css/normalize.css';
 import 'styles/styles';
 
-export default ({children}) => (
+export default (props) => (
   <div className="wrapper">
-    {children}
+    <TopBar pathname={props.location.pathname} />
+    {props.children}
   </div>
 );
