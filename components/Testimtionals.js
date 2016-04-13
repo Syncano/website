64ee20c0259454d8a31bdb150e7d944ef1e51586
Wojciech Pasiński <wpasinski@gmail.com>
2016-04-13 16:18:22 +0200

    [WEB-240] getSliderConfig(), cursor pointer for slider dots

--- components/Testimtionals.js
+++ components/Testimtionals.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import Slider from 'react-slick';
 
-export default () => {
-  const settings = {
-    arrows: false,
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+ export default React.createClass({
+  getSliderConfig() {
+    return {
+      arrows: false,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
 
-  return (
-    <div className="testimtionals">
-      <Slider {...settings}>
-        <div>
-          <div className="inner">
-            <blockquote>
-              <p>„What used to take 5 weeks now takes just 1 week with Syncano. You let developers be developers and not get stuck being middleware or database engineers.”</p>
-            </blockquote>
+  render() {
+    return (
+      <div className="testimtionals">
+        <Slider {...this.getSliderConfig()}>
+          <div>
+            <div className="inner">
+              <blockquote>
+                <p>„What used to take 5 weeks now takes just 1 week with Syncano. You let developers be developers and not get stuck being middleware or database engineers.”</p>
+              </blockquote>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="inner">
-            <blockquote>
-              <p>„What used to take 5 weeks now takes just 1 week with Syncano. You let developers be developers and not get stuck being middleware or database engineers.”</p>
-            </blockquote>
+          <div>
+            <div className="inner">
+              <blockquote>
+                <p>„What used to take 5 weeks now takes just 1 week with Syncano. You let developers be developers and not get stuck being middleware or database engineers.”</p>
+              </blockquote>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="inner">
-            <blockquote>
-              <p>„What used to take 5 weeks now takes just 1 week with Syncano. You let developers be developers and not get stuck being middleware or database engineers.”</p>
-            </blockquote>
+          <div>
+            <div className="inner">
+              <blockquote>
+                <p>„What used to take 5 weeks now takes just 1 week with Syncano. You let developers be developers and not get stuck being middleware or database engineers.”</p>
+              </blockquote>
+            </div>
           </div>
-        </div>
-      </Slider>
-    </div>
-  )
-};
+        </Slider>
+      </div>
+    )
+  }
+});
