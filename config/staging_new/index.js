4ed0ac8f1e65cd21412e9660345af47593799e48
Zhe Brah <zhebrah@gmail.com>
2016-04-13 13:19:57 +0200

    [INFRA-268] new website build added

---
+++ config/staging_new/index.js
@@ -0,0 +1,11 @@
+export default {
+  analyticsKey: 'CCupheBjgV6WI0emy3oRUnDyjQ8ngmgB',
+  apiUrl: 'https://api.syncano.rocks/',
+  dashboardUrl: 'https://dashboard.syncano.rocks/',
+  hello: {
+    github: 'cd6fd9547af702502513',
+    facebook: '946429888769674',
+    google: '263058036063-ii0k7hrmm3gigk74011jb38sq3q9t0n0.apps.googleusercontent.com',
+    redirect_uri: 'https://syncano.rocks/'
+  }
+};
