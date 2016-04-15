cf4e52935392d3bd9c3e42426f243f316c97da17
Wojciech Pasiński <wpasinski@gmail.com>
2016-04-15 13:24:46 +0200

    [WEB-263] getStatusImg method, refactoring text-panels scss

--- components/PageHeader/PageHeader.js
+++ components/PageHeader/PageHeader.js
@@ -7,18 +7,24 @@ export default ({ variant, status, headline, textlineHTML, children }) => {
     'page-header--error': (variant == 'error')
   });
 
-  let statusImg = '';
-  if (status == 'ok') {
-    statusImg = <img className="page-header__status" src={require('./images/status-ok.svg')} alt="status ok" />;
-  }
-  if (status == 'error') {
-    statusImg = <img className="page-header__status" src={require('./images/status-error.svg')} alt="status error" />;
-  }
+  const getStatusImg = () => {
+    let statusImg = '';
+
+    if (status == 'ok') {
+      statusImg = <img className="page-header__status" src={require('./images/status-ok.svg')} alt="status ok" />;
+    }
+
+    if (status == 'error') {
+      statusImg = <img className="page-header__status" src={require('./images/status-error.svg')} alt="status error" />;
+    }
+
+    return statusImg;
+  };
 
   return (
     <header className={pageHeaderClass}>
       <div className="inner">
-        {statusImg}
+        {getStatusImg()}
         <h2>{headline}</h2>
         <p dangerouslySetInnerHTML={{__html: textlineHTML}}></p>
         {children}
