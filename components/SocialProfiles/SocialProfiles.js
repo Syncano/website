d0a0a2910588a4076b43c93501d75ca93ee55da6
Wojciech Pasiński <wpasinski@gmail.com>
2016-04-16 18:48:23 +0200

    [WEB-265] spacing, empty eof, Github changed to GitHub

--- components/SocialProfiles/SocialProfiles.js
+++ components/SocialProfiles/SocialProfiles.js
@@ -5,26 +5,57 @@ export default () => {
     <div className="social-profiles">
       <div className="inner">
         <div className="social-profiles__group">
-          <h4>Fork us on Github</h4>
+          <h4>Fork us on GitHub</h4>
           <div className="social-profiles__group__profiles">
-            <a className="social-profiles__group__profiles__icon" href="#" target="_blank">
-              <img src={require('./images/github.svg')} alt="Github" />
+            <a
+              className="social-profiles__group__profiles__icon" 
+              href="#"
+              target="_blank"
+            >
+              <img
+                src={require('./images/github.svg')}
+                alt="GitHub"
+              />
             </a>
           </div>
         </div>
         <div className="social-profiles__group">
           <h4>Follow us</h4>
           <div className="social-profiles__group__profiles">
-            <a className="social-profiles__group__profiles__icon" href="#" target="_blank">
-              <img src={require('./images/facebook.svg')} alt="Facebook" />
+            <a
+              className="social-profiles__group__profiles__icon"
+              href="#"
+              target="_blank"
+            >
+              <img
+                src={require('./images/facebook.svg')}
+                alt="Facebook"
+              />
             </a>
-            <a className="social-profiles__group__profiles__icon" href="#" target="_blank">
-              <img src={require('./images/twitter.svg')} alt="Twitter" />
+            <a
+              className="social-profiles__group__profiles__icon"
+              href="#"
+              target="_blank"
+            >
+              <img
+                src={require('./images/twitter.svg')}
+                alt="Twitter"
+              />
             </a>
-            <a className="social-profiles__group__profiles__icon" href="#" target="_blank">
-              <img src={require('./images/twitter.svg')} alt="Twitter" />
+            <a
+              className="social-profiles__group__profiles__icon"
+              href="#"
+              target="_blank"
+            >
+              <img
+                src={require('./images/twitter.svg')}
+                alt="Twitter"
+              />
               <div className="social-profiles__group__profiles__icon__dot">
-                <img src={require('./images/signal.svg')} alt="" />
+                <img
+                  src={require('./images/signal.svg')}
+                  alt=""
+                />
               </div>
             </a>
           </div>
@@ -32,8 +63,15 @@ export default () => {
         <div className="social-profiles__group">
           <h4>Join the community</h4>
           <div className="social-profiles__group__profiles">
-            <a className="social-profiles__group__profiles__icon" href="#" target="_blank">
-              <img src={require('./images/slack.svg')} alt="Slack" />
+            <a
+              className="social-profiles__group__profiles__icon"
+              href="#"
+              target="_blank"
+            >
+              <img
+                src={require('./images/slack.svg')}
+                alt="Slack"
+              />
             </a>
           </div>
         </div>
