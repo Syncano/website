e9fa676d08b4069af325528156e63557b3cd69b5
Wojciech Pasiński <wpasinski@gmail.com>
2016-05-04 02:39:59 +0200

    [WEB-302] Implement text with background image section

---
+++ components/TextWithBackground/TextWithBackground.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classNames  from 'classnames';
+
+export default ({ theme, headline, textline, children }) => {
+  const textWithBackgroundClassName = classNames({
+    'text-with-background': true,
+    'text-with-background--image1': (theme == 'image1')
+  });
+
+  return (
+    <div className={textWithBackgroundClassName}>
+      <div className="inner">
+        <div className="text-with-background__text">
+          <h2>{headline}</h2>
+          <p>{textline}</p>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/TextWithBackground/index.js
@@ -0,0 +1,3 @@
+import TextWithBackground from './TextWithBackground';
+
+export default TextWithBackground;
