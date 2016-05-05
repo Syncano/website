33634987321efac870a2ae8ea5abf85541b056bd
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-05-05 15:17:00 +0200

    [WEB-297] Fix build image paths

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,24 @@
 exports.modifyWebpackConfig = function(config, env) {
+  const imageLoader = env !== 'develop' ? 'file-loader?name=/[hash].[ext]' : 'file-loader';
+
   config.removeLoader('jpg');
   config.loader('jpg', function(cfg) {
     cfg.test = /\.jpg/;
-    cfg.loader = 'file-loader?name=/[hash].[ext]';
+    cfg.loader = imageLoader;
     return cfg;
   });
 
   config.removeLoader('png');
   config.loader('png', function(cfg) {
     cfg.test = /\.png/;
-    cfg.loader = 'file-loader?name=/[hash].[ext]';
+    cfg.loader = imageLoader;
     return cfg;
   });
 
   config.removeLoader('svg');
   config.loader('svg', function(cfg) {
     cfg.test = /\.svg/;
-    cfg.loader = 'file-loader?name=/[hash].[ext]';
+    cfg.loader = imageLoader;
     return cfg;
   });
 
