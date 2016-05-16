b163f8ebe4edee5dffd70b4edf159998508c1ddb
Wojciech Pasiński <wpasinski@gmail.com>
2016-05-16 14:27:16 +0200

    [WEB-295] Implement About us page

--- config/staging_new/index.js
+++ config/staging_new/index.js
@@ -1,3 +1,7 @@
 export default {
-  analyticsKey: 'CCupheBjgV6WI0emy3oRUnDyjQ8ngmgB'
+  analyticsKey: 'huLqAgfVwOWUuWKdMtkK8kyfLhAtLYF1',
+  contactFormEmail: 'wojciech.pasinski+test762534765234@syncano.com',
+  dashboardUrl: 'https://dashboard.syncano.rocks/',
+  statusPageStatusUrl: 'https://6l1kzwgr7t06.statuspage.io/api/v2/status.json',
+  syncanoAPIBaseUrl: 'https://api.syncano.rocks/'
 };
