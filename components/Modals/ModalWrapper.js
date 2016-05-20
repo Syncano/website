7cb1361b9369389179ea5853ded1f7b661ac449b
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-05-20 09:59:31 +0200

    [WEB-338] Refactor ModalWrapper

--- components/Modals/ModalWrapper.js
+++ components/Modals/ModalWrapper.js
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import BodyClassName from 'react-body-classname';
 import classNames  from 'classnames';
 import { mouseTrap } from 'react-mousetrap';
+import _ from 'lodash';
 const isBrowser = typeof window !== 'undefined';
 
 class ModalWrapper extends Component {
@@ -11,25 +12,31 @@ class ModalWrapper extends Component {
   };
 
   handleESCShortcutBind(nextContext) {
-    const { bindShortcut, unbindShortcut, modalName } = this.props;
-    const isOpen = this.getOpenState();
-    const nextContextModalObject = nextContext.modals[modalName];
+    const { bindShortcut, unbindShortcut } = this.props;
+    const { modals } = this.context;
+    const nextContentModals = nextContext.modals;
+    const contextIsOpen = this.getModalsOpenState(modals);
+    const nextContextIsOpen = this.getModalsOpenState(nextContentModals);
 
-    if (isOpen !== nextContextModalObject.isOpen) {
-      nextContextModalObject.isOpen && bindShortcut('esc', nextContextModalObject.close);
-      !nextContextModalObject.isOpen && unbindShortcut('esc');
+    if (contextIsOpen !== nextContextIsOpen) {
+      unbindShortcut('esc');
+      nextContextIsOpen && bindShortcut('esc', nextContentModals.closeAll);
     }
   };
   
   getBodyClassName() {
     const bodyClassName = classNames({
-      'has-modal-open': this.getOpenState()
+      'has-modal-open': this.getCurrentOpenState()
     });
 
     return bodyClassName;
   };
 
-  getOpenState() {
+  getModalsOpenState(modals) {
+    return _.some(modals, { isOpen: true });
+  };
+
+  getCurrentOpenState() {
     const { modalName } = this.props;
     const { modals } = this.context;
 
@@ -45,7 +52,7 @@ class ModalWrapper extends Component {
         <Modal
           className="modal"
           overlayClassName="modal-container"
-          isOpen={this.getOpenState()}
+          isOpen={this.getCurrentOpenState()}
         >
         <span
           className="modal__close"
