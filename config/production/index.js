593489d3f7a35445f7c98358e5191cd8a8351172
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-05-25 16:37:32 +0200

    [WEB-363] Add circleCI config for production

---
+++ config/production/index.js
@@ -0,0 +1,13 @@
+export default {
+  analyticsKey: PRODUCTION_ANALYTICS_WRITE_KEY,
+  contactFormEmail: PRODUCTION_CONTACT_FORM_EMAIL,
+  dashboardUrl: PRODUCTION_DASHBOARD_URL,
+  statusPageStatusUrl: STATUS_PAGE_URL,
+  syncanoAPIBaseUrl: PRODUCTION_API_URL,
+  hello: {
+    github: PRODUCTION_HELLO_GITHUB,
+    facebook: PRODUCTION_HELLO_FACEBOOK,
+    google: PRODUCTION_HELLO_GOOGLE,
+    redirect_uri: PRODUCTION_HELLO_REDIRECT_URI
+  }
+};
