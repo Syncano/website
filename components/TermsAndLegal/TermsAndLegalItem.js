c7242fea5fb7985c5e15991252ef8f792c02c919
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-05-25 18:19:44 +0200

    [WEB-364] Fix scroll on Terms and Legal page

--- components/TermsAndLegal/TermsAndLegalItem.js
+++ components/TermsAndLegal/TermsAndLegalItem.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import _ from 'lodash';
+import { Element } from 'react-scroll';
 
 export default ({ title, children }) => {
   const slug = _.kebabCase(title);
 
   return (
-    <div
+    <Element
       key={slug}
+      name={slug}
       className="terms-and-legal__page__entry"
-      id={slug}
     >
       <h3>{title}</h3>
       {children}
-    </div>
+    </Element>
   );
 };
