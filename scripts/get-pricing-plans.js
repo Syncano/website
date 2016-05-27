57ab1bd8096745818114dccbe1269d4ad07e0131
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-05-27 18:30:38 +0200

    [WEB-369] Fix get-pricing-plans

--- scripts/get-pricing-plans.js
+++ scripts/get-pricing-plans.js
@@ -1,9 +1,13 @@
 import fs from 'fs';
 import config from '../config/';
 import axios from 'axios';
+const ENV = process.env.NODE_ENV || 'development';
 
 const fetchPlans = () => {
-  const plansUrl = `${config.syncanoAPIBaseUrl}v1.1/billing/plans/`;
+  const apiUrl = ENV === 'development' ? config.syncanoAPIBaseUrl : process.env[config.syncanoAPIBaseUrl];
+  const plansUrl = `${apiUrl}v1.1/billing/plans/`;
+
+  console.error('apiUrl', apiUrl, config.syncanoAPIBaseUrl, process.env);
 
   axios.get(plansUrl).then((response) => {
     return fs.writeFileSync(`${__dirname}/../pricing-plans.json`, JSON.stringify(response.data.objects[0]))
