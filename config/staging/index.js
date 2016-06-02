3238ed6e1435e590aeb6e956a1b6eb5937179ceb
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-06-02 14:21:15 +0200

    Test

--- config/staging/index.js
+++ config/staging/index.js
@@ -1,14 +1,14 @@
 export default {
-  analyticsKey: process.env[STAGING_ANALYTICS_WRITE_KEY],
-  contactFormEmail: process.env[STAGING_CONTACT_FORM_EMAIL],
-  dashboardUrl: process.env[STAGING_DASHBOARD_URL],
-  statusPageStatusUrl: process.env[STATUS_PAGE_URL],
-  supportFormEmail: process.env[STAGING_SUPPORT_FORM_EMAIL],
-  syncanoAPIBaseUrl: process.env[STAGING_API_URL],
+  analyticsKey: process.env['STAGING_ANALYTICS_WRITE_KEY'],
+  contactFormEmail: process.env['STAGING_CONTACT_FORM_EMAIL'],
+  dashboardUrl: process.env['STAGING_DASHBOARD_URL'],
+  statusPageStatusUrl: process.env['STATUS_PAGE_URL'],
+  supportFormEmail: process.env['STAGING_SUPPORT_FORM_EMAIL'],
+  syncanoAPIBaseUrl: process.env['STAGING_API_URL'],
   hello: {
-    github: process.env[STAGING_HELLO_GITHUB],
-    facebook: process.env[STAGING_HELLO_FACEBOOK],
-    google: process.env[STAGING_HELLO_GOOGLE],
-    redirect_uri: process.env[STAGING_HELLO_REDIRECT_URI]
+    github: process.env['STAGING_HELLO_GITHUB'],
+    facebook: process.env['STAGING_HELLO_FACEBOOK'],
+    google: process.env['STAGING_HELLO_GOOGLE'],
+    redirect_uri: process.env['STAGING_HELLO_REDIRECT_URI']
   }
 };
