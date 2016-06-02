6a8d9a307076d3bf37b5b51332d734caf61afe9e
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-06-02 14:11:38 +0200

    Test

--- config/staging/index.js
+++ config/staging/index.js
@@ -1,14 +1,14 @@
 export default {
-  analyticsKey: 'STAGING_ANALYTICS_WRITE_KEY',
-  contactFormEmail: 'STAGING_CONTACT_FORM_EMAIL',
-  dashboardUrl: 'STAGING_DASHBOARD_URL',
-  statusPageStatusUrl: 'STATUS_PAGE_URL',
-  supportFormEmail: 'STAGING_SUPPORT_FORM_EMAIL',
-  syncanoAPIBaseUrl: 'STAGING_API_URL',
+  analyticsKey: STAGING_ANALYTICS_WRITE_KEY,
+  contactFormEmail: STAGING_CONTACT_FORM_EMAIL,
+  dashboardUrl: STAGING_DASHBOARD_URL,
+  statusPageStatusUrl: STATUS_PAGE_URL,
+  supportFormEmail: STAGING_SUPPORT_FORM_EMAIL,
+  syncanoAPIBaseUrl: STAGING_API_URL,
   hello: {
-    github: 'STAGING_HELLO_GITHUB',
-    facebook: 'STAGING_HELLO_FACEBOOK',
-    google: 'STAGING_HELLO_GOOGLE',
-    redirect_uri: 'STAGING_HELLO_REDIRECT_URI'
+    github: STAGING_HELLO_GITHUB,
+    facebook: STAGING_HELLO_FACEBOOK,
+    google: STAGING_HELLO_GOOGLE,
+    redirect_uri: STAGING_HELLO_REDIRECT_URI
   }
 };
