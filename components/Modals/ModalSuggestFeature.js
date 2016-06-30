93531f00757d8dc685f3a443b459fbd2c5d0f137
Wojciech Pasiński <wpasinski@gmail.com>
2016-06-30 18:38:58 +0200

    [WEB-444] Use template string for texts using single quote

--- components/Modals/ModalSuggestFeature.js
+++ components/Modals/ModalSuggestFeature.js
@@ -19,8 +19,8 @@ export default () => {
                 <h2>Thank you!</h2>
                 <p><strong>Your message has been received.</strong></p>
                 <p>
-                  We'll get back to you soon. In the meantime,<br />
-                  take a look at our <a href="http://docs.syncano.io/" target="_blank">Docs</a>.
+                  {`We'll get back to you soon. In the meantime,`}
+                  <br />take a look at our <a href="http://docs.syncano.io/" target="_blank">Docs</a>.
                 </p>
               </div>
             }
