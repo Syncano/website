76a9ab9ef8c86eeae0305e454225a5315dd7e861
Wojciech Pasiński <wpasinski@gmail.com>
2016-10-03 15:25:02 +0200

    [WEB-539] Dashboard login screen shows when you register from website

--- components/AuthHOC/AuthHOC.js
+++ components/AuthHOC/AuthHOC.js
@@ -119,7 +119,7 @@ export default (ComposedComponent) => (
       let redirectUrl = `${APP_CONFIG.dashboardUrl}#/login?token=${token}`;
 
       if (signUpMode) {
-        redirectUrl += `&signUpMode=${signUpMode}`;
+        redirectUrl = `${APP_CONFIG.dashboardUrl}#/?token=${token}&signUpMode=${signUpMode}`;
       }
 
       window.location.href = redirectUrl;
