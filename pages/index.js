3ebfec09098f8ea959e8ce261f458362bf899981
Tomasz Ciecierski <tomek.ciecierski@syncano.com>
2017-02-09 10:53:09 +0100

    [WEB-611] Parse landing page, and Relink name change

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ const HomePage = () => {
             authorImgSrc={require('./home/stale-husby@2x.jpg')}
             quote={`If Syncano was around at the time we created our MVP, it would have saved us a full year to get into
               the market.`}
-            author="Ståle Husby, CEO, Relinklabs"
+            author="Ståle Husby, CEO, Relink"
           />
           <CardTestimonials.Card
             headline="For software developers:"
