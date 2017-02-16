afd4a1e1ea9eececd78efbd2a6d37e63e54d3f03
Tomasz Ciecierski <tomek.ciecierski@syncano.com>
2017-02-16 11:52:19 +0100

    [CLI-173] Add 2 more sections

--- static/hosting-intro/js/bashconsole.js
+++ static/hosting-intro/js/bashconsole.js
@@ -3,11 +3,11 @@ $(function() {
   const strings = [
     'yarn add syncano-cli',
     'syncano-cli init',
-    'syncano-cli hosting add <name>',
+    'syncano-cli hosting add [hosting_path]',
     'syncano-cli hosting delete <name>',
     'syncano-cli hosting list',
-    'syncano-cli hosting files <name>',
-    'syncano-cli hosting sync <name>'
+    'syncano-cli hosting files <hosting-name>',
+    'syncano-cli hosting sync <hosting-name>'
   ];
 
   codebox.typed({
