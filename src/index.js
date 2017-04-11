ce385e0f322ab1e3504f395a08b23ef8ad51e0a3
Kasper Mikiewicz <ideredpl@gmail.com>
2017-04-11 12:54:14 +0200

    fix(scripts): hotjar

--- src/index.js
+++ src/index.js
@@ -29,14 +29,14 @@ const Routes = () => (
 )
 
 Routes.init = () => {
-  {((h,o,t,j,a,r) => {
+  ((h,o,t,j,a,r) => {
       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
       h._hjSettings={hjid:108213,hjsv:5};
       a=o.getElementsByTagName('head')[0];
       r=o.createElement('script');r.async=1;
       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
       a.appendChild(r);
-  })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=')}
+  })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=')
 }
 
 export default Routes
