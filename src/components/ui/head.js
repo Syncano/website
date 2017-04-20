073a3860c9c58ccdbf79dc7f62ff1e747c8f67ca
Kasper Mikiewicz <ideredpl@gmail.com>
2017-04-20 18:21:25 +0200

    fix(component): broken open graph

--- src/components/ui/head.js
+++ src/components/ui/head.js
@@ -7,16 +7,16 @@ export default ({children, opengraph}) => (
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' />
     {children}
 
-    {opengraph === true ? (
-      <div>
-        <meta property='og:image' content='https://www.syncano.io/static/img/og-image.png' />
-        <meta property='og:title' content='Syncano means accelerated software development' />
-        <meta property='og:url' content='https://www.syncano.io/' />
-        <meta property='og:site_name' content='Syncano' />
-        <meta property='og:description' content='Don’t rebuild what has already been built. Syncano has hundreds of ready to use functions for your backend. Set up a scalable app in the cloud in just 45 seconds.' />
-        <meta property='og:type' content='website' />
-      </div>
-    ) : ''}
+    {console.log(opengraph === true)}
+
+    {opengraph === true ? ([
+      <meta key='og:image' property='og:image' content='https://www.syncano.io/static/img/og-image.png' />,
+      <meta key='og:title' property='og:title' content='Syncano means accelerated software development' />,
+      <meta key='og:url' property='og:url' content='https://www.syncano.io/' />,
+      <meta key='og:site_name' property='og:site_name' content='Syncano' />,
+      <meta key='og:description' property='og:description' content='Don’t rebuild what has already been built. Syncano has hundreds of ready to use functions for your backend. Set up a scalable app in the cloud in just 45 seconds.' />,
+      <meta key='og:type' property='og:type' content='website' />
+    ]) : ''}
 
     {React.Children.map(children, item => {
       if (item.type === 'title') {
