0e38f95350072cc4c5d3d2031eca322e7550d4de
Kasper Mikiewicz <ideredpl@gmail.com>
2017-04-27 13:29:01 +0200

    fix(service): fix auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -12,8 +12,7 @@ export default class Auth {
   }
 
   @action.bound rebuildSession () {
-    this.store.isLoggedIn = true
-    // this.store.isLoggedIn = Boolean(Cookies.get('logged_in'))
+    this.store.isLoggedIn = Boolean(Cookies.get('logged_in'))
   }
 
   @action.bound async login (credentials) {
