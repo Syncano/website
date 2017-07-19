357558c8db993cb13c94674ba863da8b8a2615e3
Adam Wardecki <adam.wardecki@syncano.com>
2017-07-19 17:36:13 +0200

    feat: add meta descriptions and title tags

--- src/views/landing/index.js
+++ src/views/landing/index.js
@@ -18,6 +18,7 @@ const Landing = ({
   <Page>
     <Head>
       <title>Syncano - Accelerated Software Development</title>
+      <meta name="Description" content="Don’t rebuild what has already been built. Syncano has hundreds of ready to use functions for your backend. Set up a scalable app in the cloud in just 45 seconds.">
     </Head>
 
     <Header theme='dark'>
